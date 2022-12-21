204883a18d02132bca3fef143880bd58b6108ad1
steveluscher <me+github@steveluscher.com>
2022-12-21 02:29:47 +0000

    Add Solana Mobile Wallet adapter to example app

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { transact } from "@solana-mobile/mobile-wallet-adapter-protocol";
+
+import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        onPress={() => {
+          transact(async (mobileWallet) => {
+            const authorization = await mobileWallet.authorize({
+              cluster: "devnet",
+              identity: { name: "My Expo App" },
+            });
+            console.log(authorization);
+          });
+        }}
+        title="Authorize"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
